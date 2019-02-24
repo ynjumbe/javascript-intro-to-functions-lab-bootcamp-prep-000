c0ff03d672e3e713404378cc282089d64d3fab3b
Yves Njumbe <ynjumbe23@gmail.com>
2019-02-24 03:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase()=== string) {
   return "I can't hear you!"
+  }
   
   if (string.toUpperCase()=== string) {
     return "YES INDEED!"
   }
-}
 }
