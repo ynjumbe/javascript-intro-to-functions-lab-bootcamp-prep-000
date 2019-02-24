d8e17dec90c136bb5dec697f6c50be47ea63ba9b
Yves Njumbe <ynjumbe23@gmail.com>
2019-02-24 03:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
   
-  if (string === "I love you, Grandma.")
-  return ""
+  if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
 }
